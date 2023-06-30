2f7d84fcf638e97abd61ceed5f0eb659a6d5f7b1
unknown <gregoryfoo@hotmail.com>
2023-06-30 18:39:16 +0800

    feat: set up server and client boilerplate

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import logger from "morgan";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.get("/api", (_req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`  App running in port ${PORT}`);
+});
