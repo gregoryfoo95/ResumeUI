c2f9fa653eefc5481035b39e2edd4000e6202acd
unknown <gregoryfoo@hotmail.com>
2023-07-21 14:40:41 +0800

    feat: Amend style of infobar

--- client/src/InnerHomePage/InnerHomePage.tsx
+++ client/src/InnerHomePage/InnerHomePage.tsx
@@ -13,8 +13,8 @@ export default function InnerHomePage(): JSX.Element {
 
     return  (
         <div className="flex flex-col gap-y-4 my-10">
+            {/* <TypeWriter appendClass="" roles={roles} prefix="" /> */}
             <InfoBar/>
-            <TypeWriter appendClass="" roles={roles} prefix="" />
             <SuccessHack/>
             <InnerHomePageWindow/>
         </div>
